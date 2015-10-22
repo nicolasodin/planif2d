e8822a07ce965b1375f4c6f5bc6e61a3d86533f0
QuentinPetit <quentin43@sfr.fr>
2015-10-22 16:21:48 +0200

    Integration ecriture PDF

--- viewers/static/getPatient.php
+++ viewers/static/getPatient.php
@@ -1,35 +1,29 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Karrach
- * Date: 21/10/2015
- * Time: 15:25
- */
- session_start();
+	session_start();
 
- header('Content-Type: text/xml');
- echo "<?xml version=\"1.0\"?>\n";
- echo "<exemple>\n";
+	header('Content-Type: text/xml');
+	echo "<?xml version=\"1.0\"?>\n";
+	echo "<exemple>\n";
 
- //on connecte a la BDD
- $dbhost="localhost";
- $dbuser="root";
- $dbpass="";
+	//on connecte a la BDD
+	$dbhost="localhost";
+	$dbuser="root";
+	$dbpass="";
 
- $nomScene = $_SESSION['nomScene'];
+	$nomScene = $_SESSION['nomScene'];
 
- $dblink=mysql_connect($dbhost,$dbuser,$dbpass);
- mysql_select_db("planif2d",$dblink);
+	$dblink=mysql_connect($dbhost,$dbuser,$dbpass);
+	mysql_select_db("planif2d",$dblink);
 
- $query="SELECT * FROM patients";
- $result=mysql_query($query,$dblink) or die (mysql_error($dblink));
- while ($row = mysql_fetch_array($result, MYSQL_NUM))
- {
-     echo "<Nom>" . $row[0] . "</Nom>\n";
-     echo "<Prenom>" . $row[1] . "</Prenom>\n";
-     echo "<NVitale>" . $row[2] . "</NVitale>\n";
-     echo "<ID>" . $row[3] . "</ID>\n";
- }
- echo "</exemple>\n";
- mysql_close($dblink);
+	$query="SELECT * FROM patients";
+	$result=mysql_query($query,$dblink) or die (mysql_error($dblink));
+	while ($row = mysql_fetch_array($result, MYSQL_NUM))
+	{
+	    echo "<Nom>" . $row[0] . "</Nom>\n";
+	    echo "<Prenom>" . $row[1] . "</Prenom>\n";
+	    echo "<NVitale>" . $row[2] . "</NVitale>\n";
+	    echo "<ID>" . $row[3] . "</ID>\n";
+	}
+	echo "</exemple>\n";
+	mysql_close($dblink);
 ?>
